ae3b6188ce8a056ae054d1a607ec8aebcf1d80cb
swarnabha <swarnabhadutta909@gmail.com>
2025-11-17 14:30:30 +0530

    feat: add reusable TradingViewWidget.jsx and integrate in app/(root)/page.jsx to show real-time stock market data

---
+++ components/TradingViewWidget.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React, { memo } from 'react';
+import useTradingWidget from "@/hooks/useTradingWidget";
+import {cn} from "@/lib/utils";
+
+const TradingViewWidget = ({title,scriptUrl,config,height=600 ,className}) =>{
+    const containerRef = useTradingWidget(scriptUrl, config, height);
+
+
+
+    return (
+        <div className="w-full">
+            {title && <h3 className="font-semibold text-2xl text-gray-100 mb-5">{title}</h3>}
+            <div className={cn('tradingview-widget-container',className)} ref={containerRef} style={{ height: "100%", width: "100%" }}>
+                <div className="tradingview-widget-container__widget" style={{ height, width: "100%" }}/>
+            </div>
+        </div>
+    );
+}
+
+export default memo(TradingViewWidget);
