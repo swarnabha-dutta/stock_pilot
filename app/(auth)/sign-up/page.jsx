053e402ac8146cd5ea5d853f1ffa84093f406a57
swarnabha <swarnabhadutta909@gmail.com>
2025-11-18 05:33:08 +0530

    feat : Implementing Authentication UI and Layout

---
+++ app/(auth)/sign-up/page.jsx
@@ -0,0 +1,160 @@
+'use client';
+
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import InputField from "@/components/forms/InputField";
+import SelectField from "@/components/forms/SelectField";
+import { INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS } from "@/lib/constants";
+import { CountrySelectField } from "@/components/forms/CountrySelectField";
+import FooterLink from "@/components/forms/FooterLink";
+// import { signUpWithEmail } from "@/lib/actions/auth.actions";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+const SignUp = () => {
+
+    const router = useRouter();
+
+    const {
+        register,
+        handleSubmit,
+        control,
+        formState: { errors, isSubmitting }
+    } = useForm({
+        defaultValues: {
+            fullName: '',
+            email: '',
+            password: '',
+            country: 'US',
+            investmentGoals: 'Growth',
+            riskTolerance: 'Medium',
+            preferredIndustry: 'Technology'
+        },
+        mode: 'onBlur',
+    });
+
+    const onSubmit = async (data) => {
+        // try {
+        //     const result = await signUpWithEmail(data);
+        //
+        //     if (result.success) {
+        //         router.push('/');
+        //     }
+        //
+        // } catch (e) {
+        //     console.error(e);
+        //     toast.error("Sign up failed", {
+        //         description:
+        //             e instanceof Error
+        //                 ? e.message
+        //                 : "Failed to create an account.",
+        //     });
+        // }
+    };
+
+    return (
+        <>
+            <h1 className="form-title">Sign Up & Personalize</h1>
+
+            <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+
+                <InputField
+                    name="fullName"
+                    label="Full Name"
+                    placeholder="John Doe"
+                    register={register}
+                    error={errors.fullName}
+                    validation={{
+                        required: "Full name is required",
+                        minLength: { value: 2, message: "Name is too short" }
+                    }}
+                />
+
+                <InputField
+                    name="email"
+                    label="Email"
+                    placeholder="contact@jsmastery.com"
+                    register={register}
+                    error={errors.email}
+                    validation={{
+                        required: "Email is required",
+                        pattern: {
+                            value: /^\w+@\w+\.\w+$/,
+                            message: "Invalid email format"
+                        }
+                    }}
+                />
+
+                <InputField
+                    name="password"
+                    label="Password"
+                    placeholder="Enter a strong password"
+                    type="password"
+                    register={register}
+                    error={errors.password}
+                    validation={{
+                        required: "Password is required",
+                        minLength: { value: 8, message: "Min 8 characters" }
+                    }}
+                />
+
+                <CountrySelectField
+                    name="country"
+                    label="Country"
+                    control={control}
+                    error={errors.country}
+                    required
+                />
+
+                <SelectField
+                    name="investmentGoals"
+                    label="Investment Goals"
+                    placeholder="Select your investment goal"
+                    options={INVESTMENT_GOALS}
+                    control={control}
+                    error={errors.investmentGoals}
+                    required
+                />
+
+                <SelectField
+                    name="riskTolerance"
+                    label="Risk Tolerance"
+                    placeholder="Select your risk level"
+                    options={RISK_TOLERANCE_OPTIONS}
+                    control={control}
+                    error={errors.riskTolerance}
+                    required
+                />
+
+                <SelectField
+                    name="preferredIndustry"
+                    label="Preferred Industry"
+                    placeholder="Select your preferred industry"
+                    options={PREFERRED_INDUSTRIES}
+                    control={control}
+                    error={errors.preferredIndustry}
+                    required
+                />
+
+                <Button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className="yellow-btn w-full mt-5"
+                >
+                    {isSubmitting
+                        ? "Creating Account…"
+                        : "Start Your Investing Journey"}
+                </Button>
+
+                <FooterLink
+                    text="Already have an account?"
+                    linkText="Sign in"
+                    href="/sign-in"
+                />
+
+            </form>
+        </>
+    );
+};
+
+export default SignUp;
