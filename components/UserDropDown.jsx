b957d3b779134c96cd732dca6c070c11530aa7af
swarnabha <swarnabhadutta909@gmail.com>
2025-11-20 08:27:00 +0530

    Implement additional more  checks of authentication logic

--- components/UserDropDown.jsx
+++ components/UserDropDown.jsx
@@ -41,7 +41,7 @@ const UserDropDown = ({user}) => {
                     <Avatar className="h-8 w-8">
                         <AvatarImage src="https://avatars.githubusercontent.com/u/176801891?v=4" alt="@shadcn" />
                         <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
-                            {user.name[0]}
+                            {user?.name?.[0] || 'U'}
                         </AvatarFallback>
                     </Avatar>
                       <div className='hidden md:flex font-medium items-start'>
@@ -57,7 +57,7 @@ const UserDropDown = ({user}) => {
                         <Avatar className="h-10 w-10">
                             <AvatarImage src="https://avatars.githubusercontent.com/u/176801891?v=4" alt="@shadcn" />
                             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
-                                {user.name[0]}
+                                {user?.name?.[0] || 'U'}
                             </AvatarFallback>
                         </Avatar>
                         <div className='flex flex-col'>
